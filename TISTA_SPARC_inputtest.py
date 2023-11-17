5fb03f0f929eb0f5f3f16c81bb3cfbb98b7fb29a
saidinesh75 <saidinesh75@gmail.com>
2023-11-17 14:17:04 +0530

    figures and results

--- TISTA_SPARC_inputtest.py
+++ TISTA_SPARC_inputtest.py
@@ -1,17 +1,3 @@
-# Trainable ISTA (TISTA)
-# 
-# This code is an implementation of Trainable ISTA (TISTA) for sparse signal recovery in Pytorch.Tensor.
-# The details of the algorithm can be found in the paper:
-# Daisuke Ito, Satoshi Takabe, Tadashi Wadayama,
-# "Trainable ISTA for Sparse Signal Recovery", arXiv:1801.01978.
-# (Computer experiments in the paper was performed with another TensorFlow implementation)
-#
-# GPU is required for exfecution of this program. If you do not have GPU,
-# just change "device = torch.device('cuda')" to 'cpu'.
-# 
-# This basic TISTA trains only $\gamma_t$.
-#
-# Last update 11/21/2018
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import numpy as np
@@ -26,32 +12,33 @@ from scipy.io import loadmat
 torch.set_default_dtype(torch.float64)
 from generate_msg_tista import generate_msg_tista
 
+import matplotlib.pyplot as plt
+dir_name = "/home/dinesh/Research_work/TISTA/model_param_evol_figures/"
+plt.rcParams["savefig.directory"] = os.chdir(os.path.dirname(dir_name))
 # device
 device = torch.device('cuda') # choose 'cpu' or 'cuda'
 
-data=loadmat("/home/saidinesh/Research_work/Dinesh_SPARC_codes_2/gold_mat_files/goldi_31")
+data=loadmat("/home/dinesh/Research_work/Dinesh_SPARC_codes_2/gold_mat_files/goldi_63.mat")
 
 A_ = np.array(data['B'])
-n,N = np.shape(A_)  # (64*4160)
+n,N = np.shape(A_)  # (64*4096)
 A = torch.from_numpy(A_)
-# N = int(n**2)
 # A_unitnorm_ = A_[:,:N]
 
 # global variables
-
-# N = 512  # length of a source signal vector
-# n = 64  # length of a observation vector
+# N = 1024  # length of a source signal vector
+# n = 64  # length of a observation vector 
 # p = 0.1  # probability for occurrence of non-zero components
 
 batch_size = 1000  # mini-batch size
-num_batch = 100  # number of mini-batches in a generation
-num_generations = 6  # number of generations
-snr =  30 # SNR for the system in dB
+num_batch = 300  # number of mini-batches in a generation
+num_generations = 5  # number of generations
+snr =  40
 
 alpha2 = 1.0  # variance of non-zero component
 alpha_std = math.sqrt(alpha2)
 max_layers = num_generations  # maximum number of layers
-adam_lr = 0.04  # initial learning parameter for Adam
+adam_lr = 0.04 # initial learning parameter for Adam
 
 learning=1
 # random seed of torch
@@ -65,10 +52,10 @@ learning=1
 L = int(4)
 M = int(N/L)
 P = 1
-p = L/N  # indicates the sparsity. The length of the message is N and the no. of nonzero values is L.
-code_params = {'P': P,    # Average codeword symbol power constraint
-                'L': L,    # Number of sections
-                'M': M,      # Columns per section
+p = 0.05       # indicates the sparsity. The length of the message is N and the no. of nonzero values is L.
+code_params = {'P': P,              # Average codeword symbol power constraint
+                'L': L,             # Number of sections
+                'M': M,             # Columns per section
                 'dist':0,
                 'n':n,
                 'EbN0_dB':5,
@@ -78,7 +65,6 @@ code_params = {'P': P,    # Average codeword symbol power constraint
                 'dist':0,
                 'K':0
                 }
-
 
 delim = torch.zeros([2,L])
 delim[0,0] = 0
@@ -151,7 +137,7 @@ global sigma_std, sigma2, xi
 network = TISTA_NET().to(device)  # generating an instance of TISTA network
 s_zero = torch.Tensor(torch.zeros(N,batch_size)).to(device)  # initial value
 opt = optim.Adam(network.parameters(), lr=adam_lr)  # setting for optimizer (Adam)
-network_path = "/home/saidinesh/Research_work/TISTA/TISTA_trained_models_new/SGT_n{n}N{N}_S{snr}_L{layers}".format(n=n, N=N,snr=snr,layers=max_layers) # gtdB_learnable means gamma, tau, delta and B are learnable
+network_path = "/home/dinesh/Research_work/TISTA/Models_testing/SGT_n{n}N{N}_S{snr}_L{layers}".format(n=n, N=N,snr=snr,layers=max_layers) # gtdB_learnable means gamma, tau, delta and B are learnable
 
 # SNR calculation
 sum = 0.0
@@ -164,9 +150,12 @@ ave = sum/(100.0 * batch_size)
 sigma2 = ave/(n*math.pow(10.0, snr/10.0))
 sigma_std = math.sqrt(sigma2)
 xi = alpha2 + sigma2
-
 # incremental training loop
 start = time.time()
+
+gamma_evol = torch.zeros(num_generations,num_batch)
+ser_prev_best = 1
+ser_best = 1
 
 for gen in (range(num_generations)):
     # training process 
@@ -174,14 +163,19 @@ for gen in (range(num_generations)):
         network.load_state_dict(torch.load(network_path))
     else:    
         for i in range(num_batch):
-            if (gen > 10): # change learning rate of Adam
-                opt = optim.Adam(network.parameters(), lr=adam_lr/50.0)
-            x = torch.Tensor(generate_msg_tista(code_params,batch_size)).to(device) 
+            gamma_evol[gen,i] = network.gamma[gen]
+            if (gen>3): # change learning rate of Adam
+                opt = optim.Adam(network.parameters(), lr=0.02)
+            # elif (gen>3):
+            #     opt = optim.Adam(network.parameters(), lr=0.01)
+
+            x = torch.Tensor(generate_msg_tista(code_params,batch_size)).to(device)
+
             # x = torch.Tensor(generate_batch()).to(device)
 
-            opt.zero_grad()
-            x_hat = network(x, s_zero, gen+1).to(device)
-            # x_hat = network(x, s_zero, max_layers).to(device)
+            opt.zero_grad() 
+            x_hat = network(x, s_zero, gen+1).to(device)            # Incremental Training
+            # x_hat = network(x, s_zero, max_layers).to(device)     # One shot Training
             loss = F.mse_loss(x_hat, x)
             loss.backward()
 
@@ -221,15 +215,47 @@ for gen in (range(num_generations)):
 
     # nmse = 10.0*math.log(nmse_sum / (tot * batch_size))/math.log(10.0) #NMSE [dB]
     avg_sec_err = sec_err_sum / tot
-
+    # if gen == 0:
+    #     ser_best = avg_sec_err 
     # print('({0}) NMSE= {1:6.3f} '.format(gen + 1, nmse))
     print('({0}) avg_sec_err_rate = {a}'.format(gen + 1,a=avg_sec_err))
+
+    if avg_sec_err < ser_best:
+        ser_prev_best = ser_best
+        ser_best = avg_sec_err
+    else:
+        print("layers trainedn exceed, num_layers needed is just {g}".format(g=gen))
+        np.savetxt("/home/dinesh/Research_work/TISTA/TISTA_results_csv/GT_n{n}N{N}_S{snr}_L{layers}_1e5_best.csv".format(n=n, N=N,snr=snr,layers=max_layers), np.array([ser_best]), delimiter=",", fmt="%.5e")
+        break
     # print('({0}) NMSE= {1:6.3f} and avg_sec_err_rate = {a}'.format(gen + 1,nmse, a=avg_sec_err))
     # end of accuracy check
  
 elapsed_time = time.time() - start
-print("elapsed_time:{0}".format(elapsed_time) + "[sec]")
+print("elapsed_time:{0} [sec] and max layers needed = {g}".format(elapsed_time,g=gen+1))
 
+# if gen == (num_generations-1):
+np.savetxt("/home/dinesh/Research_work/TISTA/TISTA_results_csv/SGT_n{n}N{N}_S{snr}_L{layers}_1e5_best.csv".format(n=n, N=N,snr=snr,layers=max_layers), np.array([ser_best]), delimiter=",", fmt="%.5e")
 torch.save(network.state_dict(),network_path)
+
+fig, ax = plt.subplots()
+ax.plot(np.arange(num_batch),gamma_evol[0,:].detach().numpy(),label='tau[{g}]'.format(g=0))
+ax.plot(np.arange(num_batch),gamma_evol[1,:].detach().numpy(),label='tau[{g}]'.format(g=1))
+ax.plot(np.arange(num_batch),gamma_evol[2,:].detach().numpy(),label='tau[{g}]'.format(g=2))
+ax.plot(np.arange(num_batch),gamma_evol[3,:].detach().numpy(),label='tau[{g}]'.format(g=3))
+# ax.plot(np.arange(num_batch),gamma_evol[4,:].detach().numpy(),label='tau[{g}]'.format(g=4))
+# ax.plot(np.arange(num_batch),gamma_evol[5,:].detach().numpy(),label='tau[{g}]'.format(g=5))
+# ax.plot(np.arange(num_batch),gamma_evol[6,:].detach().numpy(),label='tau[{g}]'.format(g=6))
+# ax.plot(np.arange(num_batch),gamma_evol[7,:].detach().numpy(),label='tau[{g}]'.format(g=7))
+# ax.plot(np.arange(num_batch),gamma_evol[8,:].detach().numpy(),label='tau[{g}]'.format(g=8))
+# ax.plot(np.arange(num_batch),gamma_evol[9,:].detach().numpy(),label='tau[{g}]'.format(g=9))
+# ax.plot(np.arange(num_batch),gamma_evol[10,:].detach().numpy(),label='tau[{g}]'.format(g=10))
+# ax.plot(np.arange(num_batch),gamma_evol[11,:].detach().numpy(),label='tau[{g}]'.format(g=10))
+
+ax.set_title('gamma_evolution_plot')              
+ax.set_xlabel('tau_value')
+ax.set_ylabel('iterations')
+plt.legend(loc="upper right")
+plt.savefig("SGT_gamma_evol_n{n}N{N}_num{num}_gen{l}_snr{s}".format(n=n,N=N,num=num_batch,l=max_layers,s=snr))  
+
 
 print("Done")
